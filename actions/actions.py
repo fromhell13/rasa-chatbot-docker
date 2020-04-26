264f2576a5fff7f888400f2b444de7c4027d5ec2
Zalizan Zolkipali <zalizan_zolkipali@astro.com.my>
2020-04-26 14:52:12 +0800

    add action joke

--- actions/actions.py
+++ actions/actions.py
@@ -1,27 +1,17 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/core/actions/#custom-actions/
+
+import requests
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+class ActionJoke(Action):
+  def name(self) -> Text:
+    return "action_joke"
+
+  def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+    request = requests.get('http://api.icndb.com/jokes/random').json()  # make an api call
+    joke = request['value']['joke']  # extract a joke from returned json response
+    dispatcher.utter_message(text=joke)  # send the message back to the user
+    return []
