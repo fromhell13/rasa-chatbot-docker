78674257d5976da2a3968dcf5885185ba7189b9a
Zalizan Zolkipali <zalizan_zolkipali@astro.com.my>
2020-04-29 14:11:22 +0800

    add validation movie stories

--- actions/actions.py
+++ actions/actions.py
@@ -9,33 +9,54 @@ logger = logging.getLogger(__name__)
 class ActionValidateStart(Action):
   def name(self):
     return "action_validate_start"
-
   def run(self, dispatcher, tracker, domain):
     message = tracker.latest_message.get('text')
-    returnSlot = None
+    validity = None
 
     if message.lower() == 'hi':
-        returnSlot = 'sayhi'
+        validity = 'sayhi'
     else:
-        validity = validateMovieCode(message)
-        if validity:
-            returnSlot = 'validmoviecode'
+        moviecode_response = validateMovieCode(message)
+        if moviecode_response is not None:
+            validity = 'validmoviecode'
         else:
-            returnSlot = 'invalid'
-    return [SlotSet('isvalidstart', returnSlot)]
+            validity = 'invalid'
+    
+    return [SlotSet('isvalidstart', validity), SlotSet('moviejson', moviecode_response)]
 
+class ActionValidateMovie(Action):
+    def name(self):
+        return "action_validate_movie"
+        
+    def run(self, dispatcher, tracker, domain):
+        movies = tracker.get_slot('moviejson')
+        validity, moviecode_response = None, None
+        if movies is not None:
+            movietitle = movies['data']['title']
+            movieprice = movies['data']['price']
+            image = movies['data']['image']
+            dispatcher.utter_template('utter_movie_info', tracker, image_url=image, movietitle=movietitle)
+        else:
+            message = tracker.latest_message.get('text')
+            moviecode_response = validateMovieCode(message)
+            if moviecode_response is not None:
+                validity = 'validmoviecode'
+                movietitle = moviecode_response['data']['title']
+                movieprice = moviecode_response['data']['price']
+                image = moviecode_response['data']['image']
+                dispatcher.utter_template('utter_movie_info', tracker, image_url=image, movietitle=movietitle)
+            else:
+                validity = 'invalid'
+
+        return [SlotSet('isvalidmoviecode', validity), SlotSet('moviejson', moviecode_response)]
+
+# Validate movie code
 def validateMovieCode(message):
-    valid = False
+    resp_json = None
     response = requests.get('http://a8525840.ngrok.io/api/movies/validate/{}'.format(message))
     if response.status_code == 200:
         resp_json = response.json()
-        movietitle = resp_json['data'][0]['title']
-        movieprice = resp_json['data'][0]['price']
-        image = resp_json['data'][0]['image']
-        SlotSet('movietitle', movietitle), SlotSet('movieprice',movieprice), SlotSet('image', image)
-        valid = True
     else:
-        SlotSet('movietitle', None), SlotSet('movieprice',None), SlotSet('image', None)
-        valid = False
+        resp_json = None
 
-    return valid
+    return resp_json
--- data/stories.md
+++ data/stories.md
@@ -4,11 +4,15 @@
   - slot{"isvalidstart" : "sayhi"}
   - utter_main_menu
 * enter_movie_code
+  - action_validate_movie
+  - slot{"isvalidmoviecode" : "validmoviecode"}
 
 ## happy path 02
 * greet
   - action_validate_start
   - slot{"isvalidstart" : "validmoviecode"}
+  - action_validate_movie
+
 
 ## sad path 01
 * greet
@@ -16,3 +20,12 @@
   - slot{"isvalidstart" : "invalid"}
   - utter_invalid_start
   - utter_main_menu
+
+## sad path 02
+* greet
+  - action_validate_start
+  - slot{"isvalidstart" : "sayhi"}
+  - utter_main_menu
+* enter_movie_code
+  - action_validate_movie
+  - slot{"isvalidmoviecode" : "invalid"}
